16ca79caed28ed61c59661fe95dec787e46e73f7
Xia <rbrian.0111@gmail.com>
2026-01-18 01:16:03 -0300

    Se agrega el proyecto
    Se agrega el proyecto de exepciones.

---
+++ SumaDosNumeros/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*
+        Console.WriteLine("Hello, World!");
+        //Hola Mundo! usando una función.
+        HolaMundo();
+        void HolaMundo();
+        {
+            Console.Write("¡Hola Mundo!");
+        }
+        */
+
+        /*
+         * Ejercicio 1: Realizar un programa que nos sume dos números y nos dé el resultado en pantalla.
+         */
+        //Declaramos las variables a usar
+        int num1, num2, resultado;
+
+        //Bienvenida
+        Console.WriteLine("Bienvenida/o, deberá ingresar 2 números para poder sumarlos!");
+        //Pedimos que ingrese los datos de las variables
+        Console.Write("Ingrese primer número: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Ahora ingrese el segundo número: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        //Sumamos los datos ingresados
+        resultado = num1 + num2;
+
+
+        //Mostramos el resultado
+        Console.WriteLine("\nEl resultado es: " + resultado);
+        //Te pide que ingreses un caracterer para salir
+        Console.Write("\n\nPor favor, para salir presione \"ENTER\" (o cualquier tecla): ");
+        Console.ReadKey();
+    }
+}
